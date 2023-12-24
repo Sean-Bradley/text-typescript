2a3cf6c75d0fba899718383b7a6bd84e0f91e444
SBCODE <seanwasere@gmail.com>
2023-12-24 16:17:27 +0000

    Create typescript.js

---
+++ text/typescript.js
@@ -0,0 +1,6 @@
+const script = document.getElementsByTagName('script')
+  for (i = 0; i < script.length; i++) {
+    if ('text/typescript' === script[i].type) {
+      const js = window.ts.transpile(script[i].innerHTML)
+      eval(js)
+    }
