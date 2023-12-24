ee476f756057977faa5378b74cd648b235c896d3
SBCODE <seanwasere@gmail.com>
2023-12-24 16:28:03 +0000

    Update typescript.js

--- text/typescript.js
+++ text/typescript.js
@@ -1,3 +1,4 @@
+const script = document.getElementsByTagName('script')
 for (i = 0; i < script.length; i++) {
     if ('text/typescript' === script[i].type) {
         const js = window.ts.transpile(script[i].innerHTML)
