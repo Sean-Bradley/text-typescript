125bb3e7257b1d67d2b8d584420499fea7059637
SBCODE <seanwasere@gmail.com>
2023-12-24 16:35:24 +0000

    Update index.js

--- text/typescript/index.js
+++ text/typescript/index.js
@@ -1,1 +1,7 @@
-
+const script = document.getElementsByTagName('script')
+for (i = 0; i < script.length; i++) {
+    if ('text/typescript' === script[i].type) {
+        const js = window.ts.transpile(script[i].innerHTML)
+        eval(js)
+    }
+}
